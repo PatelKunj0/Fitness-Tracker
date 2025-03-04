7689b252123881d7634dc61a66cf5cecd06241fd
Kunj Patel <patelkunj238@csu.fullerton.edu>
2025-03-03 18:04:44 -0800

    Login Screen: - Added a login-in button that takes you to the login page - For now, any input will let you log in - There is no way to store this data right now, so each time you log in, it resets templates.

---
+++ JS/login.js
@@ -0,0 +1,10 @@
+// ../JS/login.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("login-form").addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        // Redirect to homepage after login
+        window.location.href = "index.html"; 
+    });
+});
