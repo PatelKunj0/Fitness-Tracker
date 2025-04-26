f25a4d3a157078b612fdc8670f2184126849fca5
Kunj Patel <patelkunj238@csu.fullerton.edu>
2025-04-25 22:10:23 -0700

    Added more exercises

--- JS/exercises.js
+++ JS/exercises.js
@@ -1,15 +1,51 @@
 const exercises = [
+    // Arms
     { name: "Triceps Extension", category: "Arms" },
-    { name: "riceps Extension (Cable)", categoty: "Arms"},
+    { name: "Triceps Extension (Cable)", category: "Arms" },
     { name: "Hammer Curl (Cable)", category: "Arms" },
-
+    { name: "Bicep Curl (Dumbbell)", category: "Arms" },
+    { name: "Tricep Dip", category: "Arms" },
+    { name: "Preacher Curl (Barbell)", category: "Arms" },
+    { name: "Skull Crusher (Barbell)", category: "Arms" },
+    { name: "Hammer Curl (Dumbbell)", category: "Arms" },
+  
+    // Back
     { name: "Pull Up (Assisted)", category: "Back" },
     { name: "Lat Pulldown (Cable)", category: "Back" },
-
+    { name: "Bent Over Row (Barbell)", category: "Back" },
+    { name: "Deadlift (Barbell)", category: "Back" },
+    { name: "Cable Row", category: "Back" },
+    { name: "Single-Arm Dumbbell Row", category: "Back" },
+    { name: "T-Bar Row (Barbell)", category: "Back" },
+    { name: "Seated Wide-Grip Row", category: "Back" },
+  
+    // Chest
     { name: "Incline Chest Press (Machine)", category: "Chest" },
     { name: "Incline Bench Press (Dumbbell)", category: "Chest" },
-    
+    { name: "Push-Up", category: "Chest" },
+    { name: "Chest Fly (Dumbbell)", category: "Chest" },
+    { name: "Flat Bench Press (Barbell)", category: "Chest" },
+    { name: "Decline Bench Press (Barbell)", category: "Chest" },
+    { name: "Pec Deck Machine", category: "Chest" },
+  
+    // Legs
+    { name: "Squat (Barbell)", category: "Legs" },
+    { name: "Leg Press (Machine)", category: "Legs" },
+    { name: "Leg Extension (Machine)", category: "Legs" },
+    { name: "Leg Curl (Machine)", category: "Legs" },
     { name: "Seated Calf Raise (Plate Loaded)", category: "Legs" },
-];
-
-export default exercises;
+    { name: "Bulgarian Split Squat", category: "Legs" },
+    { name: "Romanian Deadlift (Barbell)", category: "Legs" },
+    { name: "Hack Squat (Machine)", category: "Legs" },
+  
+    // Shoulders
+    { name: "Overhead Press (Barbell)", category: "Shoulders" },
+    { name: "Lateral Raise (Dumbbell)", category: "Shoulders" },
+    { name: "Front Raise (Dumbbell)", category: "Shoulders" },
+    { name: "Arnold Press (Dumbbell)", category: "Shoulders" },
+    { name: "Reverse Fly (Machine)", category: "Shoulders" },
+    { name: "Sholder Shrugs", category: "Shoulders" }
+  ];
+  
+  export default exercises;
+  
